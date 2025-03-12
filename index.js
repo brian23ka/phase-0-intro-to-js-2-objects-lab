564674fc926b663097366617db5eb06797e60c20
Brian Kaloki <brianngenye@gmail.com>
2025-03-12 09:00:42 +0300

    my first commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// Write your solution in here
+let employee = {
+    name: "liam",
+    streetaddress: "2007 main srt",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee,[key]:value};
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function  deleteFromEmployeeByKey(employee, key){
+    let newEmployee = {...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
